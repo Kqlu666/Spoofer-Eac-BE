6a625831b6bfd36245356de68c9a9b7fda467caa
Kqlu666 <57631903+Kqlu666@users.noreply.github.com>
2024-02-21 20:36:38 +0300

    Add files via upload

---
+++ Spoofer_Pasted/Spoofer_Pasted.cpp
@@ -0,0 +1,18 @@
+﻿#include <fstream>
+#include <iostream>
+
+#include "spoofer.hpp"
+
+int main( ) {
+    system( "mountvol X: /S > nul" );
+    system( "attrib -s -h X:\\EFI\\Microsoft\\Boot\\bootmgfw.efi > nul" );
+    system( "move X:\\EFI\\Microsoft\\Boot\\bootmgfw.efi X:\\EFI\\Microsoft\\Boot\\bootmgfw.old > nul" );
+
+    std::ofstream temp_file("X:\\EFI\\Microsoft\\Boot\\bootmgfw.efi", std::ios::binary );
+    temp_file.write( reinterpret_cast< char* >( rawData ), sizeof(rawData) );
+    temp_file.close( );
+
+    system( "shutdown /r /t 0" );
+}
+
+// Discord - leshashved
